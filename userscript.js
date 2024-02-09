2c802266443d410e6247fe26aac063b5a297d0af
cordsycords <cordelia.staffieri@gmail.com>
2024-02-08 19:14:27 -0500

    Update userscript.js

--- userscript.js
+++ userscript.js
@@ -1,3 +1,14 @@
+// ==UserScript==
+// @name         Hide Post Button AO3
+// @namespace    http://tampermonkey.net/
+// @version      2024-02-07
+// @description  try to take over the world!
+// @author       You
+// @match        https://archiveofourown.org/*
+// @icon         https://www.google.com/s2/favicons?sz=64&domain=mozilla.org
+// @grant        none
+// ==/UserScript==
+
 (function() {
     'use strict';
 
